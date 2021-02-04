03cdae3681b033cea7884f7345a95aac1abb76d0
TcaciucIulian <iulian.marius83@yahoo.ro>
2021-02-04 15:03:45 +0200

    Sum 3 and 5 - for statement

---
+++ Control flow statements/NumberToWord - Switch statement/NumberToWord.java
@@ -0,0 +1,30 @@
+public class NumberToWord {
+
+    public static void printNumberInWord(int number) {
+
+        switch (number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+
+            default:
+                System.out.println("Other number");
+                break;
+        }
+    }
+}
